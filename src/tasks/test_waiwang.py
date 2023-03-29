3c7220b5915544bc106359fb55c3c42532e6d23d
songjj <songjj@zetyun.com>
2023-03-29 11:31:56 +0800

    ad

--- src/tasks/test_waiwang.py
+++ src/tasks/test_waiwang.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 url = "https://www.google.com/"
 payload = {}
@@ -15,6 +16,7 @@ headers = {
 #
 # print(response.text)
 
+print(f"文件路径是：{os.getcwd()}")
 # 读文件
 with open("waiwang_read.txt", "r", encoding="utf-8") as f:
     print(f"外网读取到的文件内容:{f.read()}")
