8fcc6a8526add5b873d691ba1e3e932ecd0ba034
1024小神 <15670339118@163.com>
2024-10-18 16:50:29 +0800

    update sifa

--- src/tasks/weixin_send.py
+++ src/tasks/weixin_send.py
@@ -3,12 +3,12 @@ import json
 import datetime
 
 
-def send_wx():
+def send_wx(params):
     url = "https://wxpusher.zjiecode.com/api/send/message"
     payload = json.dumps({
         "appToken": "AT_mLW8Ag37uQEoWZe9pjisItmIPeqKU4QN",
-        "content": "<h1>H1标题</h1><br/><p style=\"color:red;\">请进行司法签到！！！</p>",
-        "summary": "请进行司法签到",
+        "content": f"<h1>H1标题</h1><br/><p style=\"color:red;\">请进行司法签到: {params}</p>",
+        "summary": f"司法签到:{params}",
         "contentType": 2,
         "uids": [
             "UID_jhavxjntRoEkhw6xGVqSEZGNFDbD"
@@ -32,13 +32,13 @@ def main():
     print(f"今天是星期{dayOfWeek}, 当前时间是：{current_time}")
     if (current_hour == 7) and (1 <= current_minute <= 59):
         content = f"上班打卡了，亲爱的宝子们~，当前时间:{current_time}"
-        send_wx()
+        send_wx("6-8:30")
     elif (current_hour == 12) and (1 <= current_minute < 59):
         content = f"快点外卖吧，吃的胖胖的才有劲干活啊，亲爱的宝~, 当前时间:{current_time}"
-        send_wx()
+        send_wx("12-2:30")
     elif (current_hour == 20) and (1 <= current_minute < 59):
         content = f"别干了，快下班打卡吧！总是忘记打卡的人，是不是你？！！！,当前时间:{current_time}"
-        send_wx()
+        send_wx("7-9:30")
     else:
         print("不用发送打卡内容")
 
